24f0092c31f8393d08ce565d49e8e086557dc6de
Michael <Michael@10.20.145.167>
2016-03-30 15:02:35 +0200

    lille retning

--- SemesterProject/web/WEB-INF/SemesterProjectStruktur.sql
+++ SemesterProject/web/WEB-INF/SemesterProjectStruktur.sql
@@ -3,7 +3,7 @@ USE semesterproject;
 DROP TABLE if exists buildings;
 
 CREATE TABLE buildings(
-	bid INT(2),
+    bid INT(2),
     bname VARCHAR(20),
     baddress VARCHAR(20),
     parcelnumb INT(2),
