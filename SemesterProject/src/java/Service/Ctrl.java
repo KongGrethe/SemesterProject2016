081abbb64f5930054413b99fccc18e41bf7a47cb
LoD <LoD@10.20.146.187>
2016-03-30 15:23:53 +0200

    DBconnector part done

--- SemesterProject/src/java/Service/Ctrl.java
+++ SemesterProject/src/java/Service/Ctrl.java
@@ -5,10 +5,20 @@
  */
 package Service;
 
+import DataAccess.DBConnector;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author christian
  */
 public class Ctrl {
-    
+        Connection con; 
+    public Ctrl() throws ClassNotFoundException, SQLException 
+    {
+        Class.forName(DBConnector.driver);
+        con = DriverManager.getConnection(DBConnector.URL, DBConnector.ID, DBConnector.PW);
+    }
 }
