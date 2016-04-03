cae70379dafc0bd01b41b13b239d1330e8c2b91a
Michael <Michael@10.20.145.167>
2016-04-03 14:19:26 +0200

    Opdateret SemesterProjectStruktur.sql

--- SemesterProject/web/WEB-INF/SemesterProjectStruktur.sql
+++ SemesterProject/web/WEB-INF/SemesterProjectStruktur.sql
@@ -1,25 +1,63 @@
 -- CREATE DATABASE semesterproject;
+
 USE semesterproject;
+DROP TABLE if exists checkup;
 DROP TABLE if exists buildings;
 DROP TABLE if exists customer;
+DROP TABLE if exists employee;
+DROP TABLE if exists users;
 
 
-CREATE TABLE buildings(
-    bID INT(2),
-    bName VARCHAR(20),
-    bAddress VARCHAR(20),
-    parcelNr INT(2),
-    bSize DOUBLE(2,0),
-    bfPlan INT(2),
-    condLvl INT(2)
+CREATE TABLE users(
+	uID INT(1) PRIMARY KEY,
+    userName VARCHAR(10),
+    upw INT(1),
+    userRole ENUM('')
+);
+
+CREATE TABLE employee(
+	eID INT(2) PRIMARY KEY,
+    eFname VARCHAR(20),
+    eLname VARCHAR(20),
+    epw INT(2),
+    FK_uID INT(2),
+    FOREIGN KEY (FK_uID) REFERENCES users(uID)
 );
 
 CREATE TABLE customer(
-    cID INT(3),
+	cID INT(3) PRIMARY KEY,
     cFname VARCHAR(30),
     cLname VARCHAR(30),
-    cpw INT(3)
+    cpw INT(3),
+    eID INT(3) UNIQUE KEY,
+    FK_uID INT(3),
+    FK_eID INT(3),
+    FOREIGN KEY (FK_uID) REFERENCES users(uID),
+    FOREIGN KEY (FK_eID) REFERENCES employee(eID)
 );
 
-SELECT * FROM buildings;
+CREATE TABLE buildings(
+	bID INT(4) PRIMARY KEY,
+    bName VARCHAR(40),
+    bAddress VARCHAR(40),
+    parcelNr INT(4),
+    bSize DOUBLE(4,0),
+    bfPlan INT(4),
+    condLvl INT(4),
+    FK_cID INT(4),
+    FOREIGN KEY (FK_cID) REFERENCES customer(cID)
+);
+
+CREATE TABLE checkup(
+	decay VARCHAR(50),
+    FK_bID INT(5),
+    FK_eID INT(5),
+    FOREIGN KEY(FK_bID) REFERENCES buildings(bID),
+    FOREIGN KEY(FK_eID) REFERENCES employee(eID)
+);
+
+SELECT * FROM users;
+SELECT * FROM employee;
 SELECT * FROM customer;
+SELECT * FROM buildings;
+SELECT * FROM checkup;
