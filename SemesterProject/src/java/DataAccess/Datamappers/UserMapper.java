f1fd085d7480685280e6b9cf39230d9ef393b5af
christian <christian@10.20.145.253>
2016-04-05 14:55:28 +0200

    DeleteUser Metode er tilføjet til UserMapperen.

--- SemesterProject/src/java/DataAccess/Datamappers/UserMapper.java
+++ SemesterProject/src/java/DataAccess/Datamappers/UserMapper.java
@@ -15,24 +15,41 @@ import java.sql.SQLException;
  */
 public class UserMapper {
 
-    public boolean createUser(int uID, String uFName, String uLName, String upw, String email, String userRole, int FK_cuID){
+    public boolean createUser(int uID, String uFName, String uLName, String upw, String email, String userRole, int FK_cuID) {
         try {
-        String sql = "INSERT INTO ´users´ (`uID`, `uFName`, `uLName`, `upw`, `email`, `userRole`, `FK_cuID` ) VALUES(?, ?, ?, ?, ?, ?, ?)";
+            String sql = "INSERT INTO ´users´ (`uID`, `uFName`, `uLName`, `upw`, `email`, `userRole`, `FK_cuID` ) VALUES(?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement pstmt = DBConnector.getConnection().prepareStatement(sql);
-            pstmt.setInt   (1, uID);
+            pstmt.setInt(1, uID);
             pstmt.setString(2, uFName);
             pstmt.setString(3, uLName);
             pstmt.setString(4, upw);
             pstmt.setString(5, email);
             pstmt.setString(6, userRole);
-            pstmt.setInt   (7, FK_cuID);
+            pstmt.setInt(7, FK_cuID);
             pstmt.executeUpdate();
-        }catch (SQLException ex){
+        } catch (SQLException ex) {
             ex.printStackTrace();
             return false;
         }
         return true;
     }
-    
-    
+
+    public boolean deleteUser(int uID, String uFName, String uLName, String upw, String email, String userRole, int FK_cuID) {
+        try {
+            String sql = "DELETE FROM `users` WHERE (`uID`=?, `uFName`=?, `uLName`=?, `upw`=?, `email`=?, `userRole`=?, `FK_cuID`=?)";
+            PreparedStatement pstmt = DBConnector.getConnection().prepareStatement(sql);
+            pstmt.setInt(1, uID);
+            pstmt.setString(2, uFName);
+            pstmt.setString(3, uLName);
+            pstmt.setString(4, upw);
+            pstmt.setString(5, email);
+            pstmt.setString(6, userRole);
+            pstmt.setInt(7, FK_cuID);
+            pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 }
