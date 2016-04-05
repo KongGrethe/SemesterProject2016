c49d56ab7d1c2485deb37d28f96ac968fe071433
christian <christian@10.20.145.253>
2016-04-05 14:19:16 +0200

    CreateUser Metode er lavet i UserMapperen.

--- SemesterProject/src/java/DataAccess/Datamappers/UserMapper.java
+++ SemesterProject/src/java/DataAccess/Datamappers/UserMapper.java
@@ -5,10 +5,34 @@
  */
 package DataAccess.Datamappers;
 
+import DataAccess.DBConnector;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
- * @author christian
+ * @author Christian
  */
 public class UserMapper {
+
+    public boolean createUser(int uID, String uFName, String uLName, String upw, String email, String userRole, int FK_cuID){
+        try {
+        String sql = "INSERT INTO ´users´ (`uID`, `uFName`, `uLName`, `upw`, `email`, `userRole`, `FK_cuID` ) VALUES(?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement pstmt = DBConnector.getConnection().prepareStatement(sql);
+            pstmt.setInt   (1, uID);
+            pstmt.setString(2, uFName);
+            pstmt.setString(3, uLName);
+            pstmt.setString(4, upw);
+            pstmt.setString(5, email);
+            pstmt.setString(6, userRole);
+            pstmt.setInt   (7, FK_cuID);
+            pstmt.executeUpdate();
+        }catch (SQLException ex){
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    
     
 }
