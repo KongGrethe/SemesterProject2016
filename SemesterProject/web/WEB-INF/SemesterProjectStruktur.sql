5919a15cc4a669c9b8d17e3bb09fa307dc953453
Michael <Michael@10.20.145.167>
2016-04-06 14:12:59 +0200

    ændret upw fra (7) til (30) og taget noget data fra sql-struktur til sql-data

--- SemesterProject/web/WEB-INF/SemesterProjectStruktur.sql
+++ SemesterProject/web/WEB-INF/SemesterProjectStruktur.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS semesterproject;
-CREATE DATABASE semesterproject;
+-- DROP DATABASE IF EXISTS semesterproject;
+-- CREATE DATABASE semesterproject;
 
 USE semesterproject;
 DROP TABLE if exists checkup;
@@ -11,7 +11,7 @@ CREATE TABLE users(
     uID INT(7) PRIMARY KEY AUTO_INCREMENT,
     uFName VARCHAR(30),
     uLName VARCHAR(30),
-    upw VARCHAR(7),
+    upw VARCHAR(30),
     email VARCHAR(15),
     userRole ENUM('employee', 'customer'),
     FK_cuID INT(7) NOT NULL,
@@ -37,15 +37,3 @@ CREATE TABLE checkup(
     FOREIGN KEY(FK_uID) REFERENCES users(uID),
     FOREIGN KEY(FK_bID) REFERENCES buildings(bID)
 );
-
--- Hernede har jeg indsat nogle eksempelbrugere.
--- Da USERS cirkulært refererer til sig selv SKAL der være en begyndelsesbruger som alle kan henvise til, ellers virker det ikke.
--- Derfor skal i ikke slette Aron.
--- Den første værdi er 0, for AUTO_INCREMENT tager sig af den.
--- Lasse
-
-INSERT INTO users VALUES (0, 'Aron', 'Jensen', '124', 'a@b.dk', 'employee', '1');
-insert into users values(0,'Kurt','Koben','1234','z@y.com','customer','1');
-insert into users values(0,'Anette','Kodstrup','jsp','a@x.co.uk','customer','2');
-
-SELECT * FROM users;
