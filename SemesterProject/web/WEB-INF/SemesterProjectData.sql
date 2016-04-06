80e1f0c5729a498fb4cf64e068d3151287a6fb9c
Lasse <Lasse@192.168.1.13>
2016-04-06 14:02:51 +0200

    SQL Ting

--- SemesterProject/web/WEB-INF/SemesterProjectData.sql
+++ SemesterProject/web/WEB-INF/SemesterProjectData.sql
@@ -1,4 +1,6 @@
-USE semesterproject;
+--USE semesterproject;
 
-INSERT INTO users VALUES(3, "Michael", 123, 'employee');
-INSERT INTO users VALUES(5, "Christian", 122, 'customer');
+-- Disse virker ikke og skal følge formen som grundbrugerne i SemesterProjektStruktur.sql
+
+--INSERT INTO users VALUES(3, "Michael", 123, 'employee');
+--INSERT INTO users VALUES(5, "Christian", 122, 'customer');
--- SemesterProject/web/WEB-INF/SemesterProjectStruktur.sql
+++ SemesterProject/web/WEB-INF/SemesterProjectStruktur.sql
@@ -1,4 +1,5 @@
--- CREATE DATABASE semesterproject;
+DROP DATABASE IF EXISTS semesterproject;
+CREATE DATABASE semesterproject;
 
 USE semesterproject;
 DROP TABLE if exists checkup;
@@ -7,7 +8,7 @@ DROP TABLE if exists users;
 
 
 CREATE TABLE users(
-    uID INT(7) PRIMARY KEY,
+    uID INT(7) PRIMARY KEY AUTO_INCREMENT,
     uFName VARCHAR(30),
     uLName VARCHAR(30),
     upw VARCHAR(7),
@@ -37,6 +38,14 @@ CREATE TABLE checkup(
     FOREIGN KEY(FK_bID) REFERENCES buildings(bID)
 );
 
+-- Hernede har jeg indsat nogle eksempelbrugere.
+-- Da USERS cirkulært refererer til sig selv SKAL der være en begyndelsesbruger som alle kan henvise til, ellers virker det ikke.
+-- Derfor skal i ikke slette Aron.
+-- Den første værdi er 0, for AUTO_INCREMENT tager sig af den.
+-- Lasse
+
+INSERT INTO users VALUES (0, 'Aron', 'Jensen', '124', 'a@b.dk', 'employee', '1');
+insert into users values(0,'Kurt','Koben','1234','z@y.com','customer','1');
+insert into users values(0,'Anette','Kodstrup','jsp','a@x.co.uk','customer','2');
+
 SELECT * FROM users;
-SELECT * FROM buildings;
-SELECT * FROM checkup;
