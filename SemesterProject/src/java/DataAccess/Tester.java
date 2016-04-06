7e7b8178bd23fafd92be72d579cb27f240449037
Michael <Michael@10.20.145.167>
2016-04-06 14:51:32 +0200

    Connection Tester

---
+++ SemesterProject/src/java/DataAccess/Tester.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataAccess;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Michael
+ */
+public class Tester {
+    public static void main(String[] args) {
+        try {
+         //DBFacade dbf = DBFacade.getInstance();
+         Connection con = DBConnector.getConnection();
+         PreparedStatement pstmt = con.prepareStatement("SELECT * FROM users");
+         ResultSet rs = pstmt.executeQuery();
+         while (rs.next()) {
+         int uID = rs.getInt(1);
+         String uFName = rs.getString("uFName");
+         String uLName = rs.getString("uLName");
+         String upw = rs.getString("upw");
+         String email = rs.getString("email");
+         String userRole = rs.getString("userRole");
+         int cuID = rs.getInt(1);
+         System.out.println(uID + "\t" + uFName + "\t" + uLName + "\t" + upw + "\t" + email + "\t" + userRole + "\t" + cuID);
+         }
+         } catch (SQLException ex) {
+         ex.printStackTrace();
+         }
+    }
+}
