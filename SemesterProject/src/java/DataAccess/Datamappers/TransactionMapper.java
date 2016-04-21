b046cf06c0b1562b0c4357aa911a44538e71bbb8
Michael <Michael@10.20.163.147>
2016-04-21 12:03:49 +0200

    TransactionMapper lavet lidt mere på, har checket om det virker(hvilket det gør)
    semesprojectstruktur har ændret checkup fra 50 til 500 tegn.

--- SemesterProject/src/java/DataAccess/Datamappers/TransactionMapper.java
+++ SemesterProject/src/java/DataAccess/Datamappers/TransactionMapper.java
@@ -5,6 +5,13 @@
  */
 package DataAccess.Datamappers;
 
+import DataAccess.DBConnector;
+import static DataAccess.DBConnector.getConnection;
+import Service.DataException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author Michael
@@ -12,4 +19,36 @@ package DataAccess.Datamappers;
 public class TransactionMapper {
     //insert checkup rapport
     //update checkup liste
+
+    public static String driver = "com.mysql.jdbc.Driver";
+    public static String URL = "jdbc:mysql://localhost/semesterproject"; //"jdbc:mysql://localhost/JDBCWebDB" ;
+    public static String ID = "root";
+    public static String PW = "root";
+
+    public boolean Transaction(Connection dbcon) throws DataException {
+        Connection dbCon = null;
+        PreparedStatement preparedStatementInsert = null;
+        PreparedStatement preparedStatementUpdate = null;
+
+        String insertTableSQL = "INSERT INTO checkup"
+                + "(checkupid, checkName, checkAddress, checkPostnr, checkDato, checkFileName, checkBygÅr, "
+                + "checkBygAreal, checkFormål, checkTag, checkYderVæg, TagFilNavn, yVægFilNavn, checkDecay "
+                + "checkUdfør, checkAnsvar, FK_uID, FK_bID) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+        String updateTableSQL = "UPDATE checkup SET checkName =? "
+                + "WHERE checkupid = ?";
+
+        try {
+            dbCon = getConnection();
+        
+            dbCon.setAutoCommit(false);
+            
+            preparedStatementInsert = dbCon.prepareStatement(insertTableSQL);
+            
+        
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return true;
+    }
 }
