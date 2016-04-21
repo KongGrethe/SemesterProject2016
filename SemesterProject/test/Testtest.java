cffd6a7261f9cc52b117cd6fec55fe16cca2e0a3
Joachim <Joachim@10.20.171.182>
2016-04-21 12:05:37 +0200

    Testet nogle metoder.

--- SemesterProject/test/Testtest.java
+++ SemesterProject/test/Testtest.java
@@ -1,6 +1,8 @@
 import DataAccess.Datamappers.BuildingMapper;
 import Service.DataException;
+import Service.Entity.User;
 import Service.EntityFacade;
+import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -9,7 +11,7 @@ import static org.junit.Assert.*;
  */
 public class Testtest {
     
-    
+    /*
     @Test
     public void addBuilding() {
         BuildingMapper bm = new BuildingMapper();
@@ -24,5 +26,12 @@ public class Testtest {
     public void deleteUser() throws ClassNotFoundException, DataException {
         EntityFacade ef = new EntityFacade();
         assertTrue(ef.deleteUser(4));
+    }*/
+    @Test
+    public void getUsers() throws DataException {
+        EntityFacade ef = new EntityFacade();
+        List<User> TestList = ef.selectUsers();
+        System.out.println(TestList.get(0).getuFName());
+        assertTrue(TestList.size() > 0);
     }
 }
